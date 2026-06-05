003a22bc2164fcf1173733117f5f782386bb5a3d
Mythreyi S <mythreyishekhar3103@gmail.com>
2026-06-05 19:38:07 +0530

    Update machine_learning.py

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -1,1 +1,16 @@
+import joblib
 
+model = joblib.load("models/career_predictor.pkl")
+scaler = joblib.load("models/scaler.pkl")
+label_encoder = joblib.load("models/label_encoder.pkl")
+
+
+def predict_career(user_input):
+    
+    scaled_input = scaler.transform([user_input])
+
+    prediction = model.predict(scaled_input)
+
+    career = label_encoder.inverse_transform(prediction)
+
+    return career[0]
